43eb9200e46c469cd431fb2c1d4c0746950ae7a3
Kai Middleton <kai.middleton@gmail.com>
2017-11-05 21:08:13 -0800

    the basic lunch.rb script now builds groups correctly

--- lunch.rb
+++ lunch.rb
@@ -3,12 +3,9 @@
 # Program to create random lunch groups
 
 # INSTRUCTIONS:
-# add names or remove names from the "people" block below
-# change the group size if you like
+# add, change, or remove names from the "people" block below if you need to
 # run the Program
 # it will give a random result each time
-
-size = 5
 
 people = <<-PUT_NAMES_IN_THIS_BLOCK
 Heather
@@ -17,44 +14,7 @@ Melvina
 Layne
 Jannie
 Arjun
-Ima
-Verdie
-Bria
-Princess
-Dock
-Breanne
-Brian
-Deontae
-Manuel
-Lessie
-Jada
-Sheridan
-Herbert
-Christian
-Lula
-Aurelio
-Kyle
-Destiny
-Elna
-Benjamin
-Fiona
-Mathias
-Tyrell
-Sheldon
-Enrico
-Darrel
-Jordi
-Carlo
-Anais
-Ronny
-Bertrand
-Gunner
-Emiliano
-Jarred
-Karine
-Evalyn
-Haylee
-Hannah
+Anna-Maija
 Humberto
 Neva
 Conner
@@ -63,36 +23,49 @@ Kaitlyn
 Nicholaus
 Belle
 Sibyl
-Leone
-Amber
+Vishwanathan
+Yong
 Jocelyn
 Percival
-Izabella
-Maximus
-Ross
 Gabe
 Brando
 Cielo
-Christiana M.
 PUT_NAMES_IN_THIS_BLOCK
 
 
 # Do not modify the code below this line unless you know what you're doing
-names = people.split("\n").to_a.shuffle
-grouping = names.each_slice(size).to_a
-alternate_grouping = nil
-if names.length % size != 0
-  for alternate_size in [size - 1, size + 1, size +2] do
-    if names.length % alternate_size == 0
-      alternate_grouping = names.each_slice(alternate_size).to_a
-    end
+
+def rebalance groups
+  last = groups.last
+  if last.length % 4 == 1
+    groups[0] << last.pop
+    groups.pop
+  elsif last.length % 4 == 2
+    groups[0] << last.pop
+    groups[1] << last.pop
+    groups.pop
   end
+  groups
 end
-display_them = Proc.new {|group, i| puts "group#{i+1}: #{group.join(', ')}"}
-puts "Lunch groups with #{size} people each"
-grouping.each_with_index &display_them
-if alternate_grouping
-  puts
-  puts "Here are alternate same-size groups"
-  alternate_grouping.each_with_index &display_them
+
+def display groups
+  groups.each_with_index {|group, i| puts "group#{i+1}: #{group.join(', ')}"}
+end
+
+names = people.split("\n").shuffle
+
+if names.length == 0
+  puts "No lunch groups yet as we have no people in the list"
+elsif names.length <= 5
+  puts "Here is your one lunch group:"
+  groups = names.each_slice(5).to_a
+  display groups
+elsif names.length == 6
+  puts "Here are two groups of three for your dining pleasure"
+  groups = names.each_slice(3).to_a
+  display groups
+else
+  groups = names.each_slice(4).to_a
+  puts "Here are your lunch groups:"
+  display rebalance(groups)
 end
